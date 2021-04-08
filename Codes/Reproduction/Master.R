c5740113f47ee508d6232a180e960ef5de7de13e
Abdoulaye Cisse <cisse@berkeley.edu>
2021-04-08 12:13:24 +0000

    Updating the master file

--- Codes/Reproduction/Master.R
+++ Codes/Reproduction/Master.R
@@ -7,36 +7,35 @@ setwd("/Users/abdoulayecisse/Desktop/Revised-reproduction-package-for-Acemoglu-e
 options("RStata.StataPath" = "/Applications/Stata/StataSE.app/Contents/MacOS/stata-se")
 options("RStata.StataVersion" = 16)
 
-
 #Running the do file for Table 2 of the paper
-stata('Table2_final_copy.do')
+stata('Table2_final.do')
 
 #Running the do file for Table 3 of the paper
-stata('Table3_final_copy.do')
+stata('Table3_final.do')
 
 #Running the do file for Table 4 of the paper
-stata('Table4_final_copy.do')
+stata('Table4_final.do')
 
 #Running the do file for Table 5 of the paper
-stata('Table5_final_copy.do')
+stata('Table5_final.do')
 
 #Running the do file for Table 6 of the paper
-stata('Table6_final_copy.do')
+stata('Table6_final.do')
 
 #Running the do file for Table 7 of the paper
-stata('Table7_final_copy.do')
+stata('Table7_final.do')
 
 #Running the do file for Table 8 of the paper
-stata('Table8_final_copy.do')
+stata('Table8_final.do')
 
 #Running the do file for Figure 1 of the paper
-stata('Figure1_final_copy.do')
+stata('Figure1_final.do')
 
 #Running the do file for Figure 2 of the paper
-stata('FIgure2_final_copy.do')
+stata('FIgure2_final.do')
 
 #Running the do file for Figure 3, 4 and 5 of the paper
-stata('Figure3_4_5_final_copy.do')
+stata('Figure3_4_5_final.do')
 
 #Running the do file for Figure 6 of the paper
-stata('Figure6_final_copy.do')
+stata('Figure6_final.do')
